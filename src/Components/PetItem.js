74ac659e32e00d197583f8ac1cdb914d0bc9571a
eng-ahmad-alabdulrazaq <eng.ahmad.alabdulrazaq@gmail.com>
2023-12-24 15:47:43 +0300

    task solved

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,11 +1,32 @@
+//\\ بسم الله الرحمن الرحيم //\\
+
+// In PetItem.js, create a state for the pet image with an inital value of pet.image.
+// Pass it to the image's src tag.
+// Add an onClick to the button that changes the state from "pet.image to "pet.image2"
+// Test your code, the button should change the image to a gif!
+
+import React, { useState } from "react";
+
 function PetItem({ pet }) {
+  // mywork
+  const [petImage, setPetImage] = useState(pet.image);
+  const changePetImage = () => {
+    setPetImage(pet.image2);
+  };
+
+  // mywork
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={petImage} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={changePetImage}
+          >
             Pet
           </button>
           <button type="button" class="btn btn-info  m-2">
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,8 +1,64 @@
+//\\ بسم الله الرحمن الرحيم //\\
+
+// In PetsList, create a state called query and a method
+// called setQuery.
+// In the input tag of SearchBar add an onChange method
+// that calls setQuery() and gives it event.target.value as
+// an argument.
+// At this point, whatever you type in your search bar
+// should appear in the query state in React Dev Tools.
+// In your PetsList, filter your pets by comparing your
+// pet's name with query using includes before you map your
+// list so you can deal with plain javascript object.
+// At this point, our search bar is case sensitive.
+// Fix it so that it becomes case insensitive.
+
+// Filter Pets by specie
+// in PetsList, create a state called type and a method called setType.
+// In the select tag of PetSelector add an onChange method that calls setType() and gives it event.target.value as an argument.
+// At this point, whatever you select in your selector should appear in the type state in React Dev Tools.
+// In your PetsList, filter your pets by comparing your pet's type with type using includes.
+// Challenge
+// Move all your search bar code to it's own component and pass setQuery as a prop to your new component.
+// Move all your selector code to it's own component and pass setType as a prop to your new component.
+
+import { useState } from "react";
 import pets from "../petsData";
 import PetItem from "./PetItem";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  // mywork
+  const [type, setType] = useState("");
+  const select = (e) => {
+    setType(e.target.value);
+  };
+  const [query, setQuery] = useState("");
+  const search = (e) => {
+    setQuery(e.target.value);
+  };
+
+  console.log(query);
+  // const changeSetQuery = (e) => {
+  //   console.log(e.target.value);
+  //   setQuery(e.target.value);
+  // };
+
+  // const petList.)includes(query)
+  // mywork
+  const petList = pets
+    .filter((pet) =>
+      pet.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    )
+    .filter((pet) => pet.type.includes(type))
+    .map((pet) => <PetItem pet={pet} key={pet.id} />);
+  // mywork
+
+  // mywork
+  // const [type, setType] = useState("");
+  // const select = (e) => {
+  //   setType(e.target.value);
+  // };
+  // mywork
 
   return (
     <section id="doctors" className="doctor-section pt-140">
@@ -20,11 +76,16 @@ function PetsList() {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  // my work
+                  // onChange={(e) => setQuery(e.target.value)}
+                  // onChange changeSetQuery(event.target.value)
+                  onChange={search}
+                  // my work
                 />
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select className="form-select" onChange={select}>
                 <option value="" selected>
                   All
                 </option>
